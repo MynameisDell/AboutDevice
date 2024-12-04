225ece3f5e7142f44d6a7369fbbf833e822ebe83
Dell <93853674+MynameisDell@users.noreply.github.com>
2024-12-04 12:31:34 +0700

    Update main.m

--- main.m
+++ main.m
@@ -4,59 +4,58 @@
 #include <CoreFoundation/CoreFoundation.h>
 #include <MobileGestalt/MobileGestalt.h>
 
+#define kMGDeviceName CFSTR("DeviceName")
 #define kMGBuildVersion CFSTR("BuildVersion")
 #define kMGProductType CFSTR("ProductType")
 #define kMGSerialNumber CFSTR("SerialNumber")
 #define kMGWifiAddress CFSTR("WifiAddress")
 #define kMGBluetoothAddress CFSTR("BluetoothAddress")
 #define kMGProductVersion CFSTR("ProductVersion")
+#define kMGUniqueDeviceID CFSTR("UniqueDeviceID")
+#define kMGRegionCode CFSTR("RegionCode")
 
 typedef struct {
     const char *attribute;
     CFStringRef key;
 } MGAttribute;
 
-MGAttribute MGAttributeVersion = {"VERSION", kMGBuildVersion};
-MGAttribute MGAttributeProduct = {"PRODUCT", kMGProductType};
-MGAttribute MGAttributeSerialNumber = {"SERIAL_NUMBER", kMGSerialNumber};
-MGAttribute MGAttributeWifiAddress = {"WIFI_ADDRESS", kMGWifiAddress};
-MGAttribute MGAttributeBluetoothAddress = {"BLUETOOTH_ADDRESS", kMGBluetoothAddress};
-MGAttribute MGAttributeSoftwareVersion = {"SOFTWARE_VERSION", kMGProductVersion};
+MGAttribute attributes[] = {
+    {"DEVICE_NAME", kMGDeviceName},
+    {"VERSION", kMGBuildVersion},
+    {"PRODUCT", kMGProductType},
+    {"SERIAL_NUMBER", kMGSerialNumber},
+    {"WIFI_ADDRESS", kMGWifiAddress},
+    {"BLUETOOTH_ADDRESS", kMGBluetoothAddress},
+    {"SOFTWARE_VERSION", kMGProductVersion},
+    {"UNIQUE_DEVICE_ID", kMGUniqueDeviceID},
+    {"REGION_CODE", kMGRegionCode}
+};
 
 void MGSendRequest(MGAttribute attributes[], size_t numAttributes) {
+    printf("\n=== DEVICE INFORMATION ===\n");
     for (size_t i = 0; i < numAttributes; i++) {
         CFStringRef result = MGCopyAnswer(attributes[i].key, NULL);
         
         if (result != NULL) {
-            const char *resultStr = CFStringGetCStringPtr(result, kCFStringEncodingUTF8);
-            if (resultStr == NULL) {
-                char buffer[1024];
-                if (CFStringGetCString(result, buffer, sizeof(buffer), kCFStringEncodingUTF8)) {
-                    resultStr = buffer;
-                }
-            }
+            char buffer[1024];
+            const char *resultStr = CFStringGetCString(result, buffer, sizeof(buffer), kCFStringEncodingUTF8) ? buffer : NULL;
+
             if (resultStr != NULL) {
-                printf("%s: %s\n", attributes[i].attribute, resultStr);
+                printf("%-20s: %s\n", attributes[i].attribute, resultStr);
+            } else {
+                printf("%-20s: [Unable to convert data]\n", attributes[i].attribute);
             }
 
             CFRelease(result);
         } else {
-            printf("%s: Không thể lấy dữ liệu\n", attributes[i].attribute);
+            printf("%-20s: [Unable to retrieve data]\n", attributes[i].attribute);
         }
     }
+    printf("=============================\n");
 }
 
 int main(int argc, char **argv) {
-    MGAttribute attributes[] = {
-        MGAttributeVersion,
-        MGAttributeProduct,
-        MGAttributeSerialNumber,
-        MGAttributeWifiAddress,
-        MGAttributeBluetoothAddress,
-        MGAttributeSoftwareVersion
-    };
-
-    MGSendRequest(attributes, sizeof(attributes) / sizeof(attributes[0]));
-
+    size_t numAttributes = sizeof(attributes) / sizeof(attributes[0]);
+    MGSendRequest(attributes, numAttributes);
     return 0;
 }
